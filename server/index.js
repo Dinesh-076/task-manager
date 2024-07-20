f9bfb9f4c77063e17edd0d03a3f4f673a5b6da8e
Dinesh-076 <dheenaraven076@gmail.com>
2024-07-21 01:38:23 +0530

    Hotfix 2.0

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ const app = express();
 const port = 4000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://task-manager-dinesh.netlify.app',
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
